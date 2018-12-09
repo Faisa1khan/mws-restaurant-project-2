d100b7728f5e6aefb5b1366610a8d6dfdbfb1a88
faisa1khan <faisalkham5464@gmail.com>
2018-12-09 22:47:25 +0530

    first commit

--- js/register.js
+++
@@ -1,6 +0,0 @@
-/* Set up service worker */
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/sw.js')
-           .then(function() { console.log("Service Worker Registered"); });
-}
